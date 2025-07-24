b4241292010991c53cf61634c59470d64714f634
Mishra Aditi Rakesh <mishra_aditi_rakesh_from.tp@tech.gov.sg>
2025-07-24 15:59:23 +0800

    fixed firebase issues

--- src/pages/SubmitUseCasePage.tsx
+++ src/pages/SubmitUseCasePage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { ArrowLeft } from 'lucide-react'
-import HackathonForm from '../components/HackathonForm'
+import HackathonForm from '../components/HackV2'
 
 const SubmitUseCasePage = () => {
   return (
